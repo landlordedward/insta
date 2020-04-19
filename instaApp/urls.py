55af34bca21302fd9846b4ee3725c4b77ef579b9
Eddie <landlordedward@gmail.com>
2020-04-19 16:08:26 -0700

    Add code to display static page.

--- instaApp/urls.py
+++ instaApp/urls.py
@@ -1,4 +1,4 @@
-"""instaJZ URL Configuration
+"""instaApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from instaApp.views import HelloWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('insta/', include('instaApp.urls')),
+    path('', HelloWorld.as_view(), name='home'),
 ]
